5058e09da38d68735cbd9f80a43b9e97e1872bab
yangfcm <yangfcm@outlook.com>
2023-03-09 00:09:05 +1030

    Init project

--- config/settings.js
+++
@@ -1,9 +0,0 @@
-const settings = {
-  POSTS_PER_PAGE: 5,
-  COMMENTS_PER_PAGE: 5
-};
-
-module.exports = {
-  POSTS_PER_PAGE: settings.POSTS_PER_PAGE,
-  COMMENTS_PER_PAGE: settings.COMMENTS_PER_PAGE
-};
