614cdc855697aa44199e3596d85da211e856e08f
yangfcm <yangfcm@outlook.com>
2023-03-10 11:52:03 +1030

    Generate auth token.

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 import { UserSignupData, UserData } from "../dtos/user";
 import { createUser } from "../repositories/user";
 import parseError, { APIError } from "../utils/parseError";
+import generateAuthToken from "../utils/generateAuthToken";
 
 const router = Router();
 
@@ -18,7 +19,8 @@ router.post(
   ) => {
     try {
       const user = await createUser(req.body);
-      res.json(user);
+      const token = generateAuthToken(user);
+      res.header("X-Auth", token).json(user);
     } catch (err: any) {
       res.status(400).json(parseError(err));
     }
