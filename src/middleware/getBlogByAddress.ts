40b4396e00d648b5f355d8c3bd78eeb11da5c571
yangfcm <yangfcm@outlook.com>
2023-03-15 20:51:42 +1030

    Consider if address param is unavailable in getBlogByAddress middleware.

--- src/middleware/getBlogByAddress.ts
+++ src/middleware/getBlogByAddress.ts
@@ -3,12 +3,16 @@ import { readBlogByAddress } from "../repositories/blog";
 import { BLOG } from "../settings/constants";
 
 const getBlogByAddress = async (
-  req: Request<{ address: string }>,
+  req: Request<{ address?: string }>,
   res: Response,
   next: NextFunction
 ) => {
-  const blog = await readBlogByAddress(req.params.address);
+  const { address } = req.params;
+  if (!address) return res.status(404).send({ message: BLOG.NOT_FOUND });
+
+  const blog = await readBlogByAddress(address);
   if (!blog) return res.status(404).send({ message: BLOG.NOT_FOUND });
+
   req.body.blog = blog;
   next();
 };
