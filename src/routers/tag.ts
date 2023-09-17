94f934cccf9b8855508c3a0edd31e01b5da607ae
yangfcm <yangfcm@outlook.com>
2023-09-17 10:35:22 +0930

    Implement update and delete tag.

--- src/routers/tag.ts
+++ src/routers/tag.ts
@@ -3,7 +3,14 @@ import { TagData, TagPostData } from "../dtos/tag";
 import authenticate from "../middleware/authenticate";
 import userOwnsBlog from "../middleware/userOwnsBlog";
 import parseError, { APIError } from "../utils/parseError";
-import { createTag, readTagsByBlogId } from "../repositories/tag";
+import {
+  createTag,
+  deleteTag,
+  findTagByName,
+  readTagsByBlogId,
+  updateTag,
+} from "../repositories/tag";
+import { TAG } from "../settings/constants";
 
 type TagResponse =
   | {
@@ -49,6 +56,8 @@ router.post(
     try {
       const { blog } = req;
       const { tag } = req.body;
+      const foundTag = await findTagByName(tag.name, blog?.id);
+      if (foundTag) throw new Error(TAG.NAME_IN_USE);
       const newTag = await createTag({
         ...tag,
         blogId: blog?.id,
@@ -63,11 +72,54 @@ router.post(
 /**
  * Update a tag
  */
-router.put("/:tagId", [authenticate, userOwnsBlog], async () => {});
+router.put(
+  "/:tagId",
+  [authenticate, userOwnsBlog],
+  async (
+    req: Request<
+      { address?: string; blogId?: string; tagId: string },
+      any,
+      { tag: Partial<TagPostData> }
+    >,
+    res: Response<TagResponse | APIError>
+  ) => {
+    try {
+      const { blog } = req;
+      const { tagId } = req.params;
+      const { tag } = req.body;
+      if (tag.name && blog?.id) {
+        const foundTag = await findTagByName(tag.name, blog.id);
+        if (foundTag && foundTag.id !== tagId) {
+          throw new Error(TAG.NAME_IN_USE);
+        }
+      }
+      const updatedTag = await updateTag(tagId, tag, blog?.id);
+      if (!updatedTag) return res.status(404).send();
+      res.json({
+        tag: updatedTag,
+      });
+    } catch (err) {
+      res.status(500).json(parseError(err));
+    }
+  }
+);
 
 /**
  * Delete a blog
  */
-router.delete("/:tagId", [authenticate, userOwnsBlog], async () => {});
+router.delete(
+  "/:tagId",
+  [authenticate, userOwnsBlog],
+  async (
+    req: Request<{ address?: string; blogId?: string; tagId: string }>,
+    res: Response<TagResponse | APIError>
+  ) => {
+    try {
+      const deletedTag = await deleteTag(req.params.tagId);
+      if (!deletedTag) return res.status(404).send();
+      res.json({ tag: deletedTag });
+    } catch (err: any) {}
+  }
+);
 
 export default router;
